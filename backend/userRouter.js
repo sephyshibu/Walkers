81e9582944b117fa9bfd806f2b58402d7693ab7b
sephyshibu <sephyshibu@gmail.com>
2025-05-17 12:11:02 +0530

    user search with debounce is completed

--- backend/userRouter.js
+++ backend/userRouter.js
@@ -1,3 +1,136 @@
+// const express = require('express');
+// const router = express.Router();
+// const {
+//     verifypaymentwallet, createrazorpay, placeorderbywallet, couponamount, preverifypayment, products,
+//     retryupdateproduct, verifyretrypayment, fetchparticularorder, sortoptionorders, searchoption,
+//     fetchwallet, coupondetails, applycoupon, fetchcoupon, returnorder, removeproductfrowwishlist,
+//     fetchwishlist, verifyPayment, updatepasswordemail, forgetpasswordresendotp, forgetpasswordverifyotp,
+//     checkemail, updateuserdetail, deleteitem, deleteorder, checkout, placingorder, fetchdefaultaddress,
+//     changepassword, updateStatus, deleteaddress, updateaddress, fetechspecificaddress, fetchaddress,
+//     addaddress, updatecartminus, updatecartplus, fetchcart, addcart, refreshToken, categoryname,
+//     fetchrecom, getProducts, signup, verifyotp, resendotp, googleLogin, login, fetchproductdetails,
+//     fetchorder, addwishlist
+// } = require('./Controller/userController');
+
+// const verifyAccessToken = require('./middleware/verifyaccessToken');
+// const checkusersstatus = require('./middleware/checkuserstatus');
+// const searchLimiter = require('./middleware/searchlimiter');
+
+// // Public Routes (No Token Required)
+// router.post('/signup', signup);
+// router.post('/refresh', refreshToken);
+// router.post('/login', login);
+// router.post('/verifyotp', verifyotp);
+// router.post('/forgetpasswordverifyotp', forgetpasswordverifyotp);
+// router.post('/resendotp', resendotp);
+// router.put('/updatepasswordemail', updatepasswordemail);
+// router.post('/forgetpasswordresendotp', forgetpasswordresendotp);
+// router.post('/auth/google', googleLogin);
+// router.post('/checkemail', checkemail);
+// router.get('/fetchcategory', categoryname);
+// router.get('/fetchbestproducts', products);
+
+// // Protected Routes (Require Access Token)
+// router.use(verifyAccessToken);
+
+// // Product Routes
+// router.get('/getproducts', getProducts);
+// router.get('/products/display/:id', fetchproductdetails);
+// router.get('/products/recommendations/:category', fetchrecom);
+
+// // Cart Routes
+// router.post('/addcart', checkusersstatus, addcart);
+// router.get('/fetchcart/:userId', checkusersstatus, fetchcart);
+// router.put('/updatecartminus/:userId', updatecartminus);
+// router.put('/updatecartplus/:userId', updatecartplus);
+
+// // Address Routes
+// router.post('/addaddress', addaddress);
+// router.get('/fetchaddress/:userId', checkusersstatus, fetchaddress);
+// router.get('/fetechspecificaddress/:id', fetechspecificaddress);
+// router.put('/updateaddress/:id', updateaddress);
+// router.delete('/deleteaddress/:id', deleteaddress);
+// router.get('/fetchdefaultaddress/:userId', checkusersstatus, fetchdefaultaddress);
+
+// // User Management Routes
+// router.put('/updatepassword/:id', checkusersstatus, changepassword);
+// router.put('/userupdate/:userId', updateuserdetail);
+
+// // Order Routes
+// router.post('/placeorder', checkusersstatus, placingorder);
+// router.post('/checkout', checkusersstatus, checkout);
+// router.get('/fetchorder/:userId', checkusersstatus, fetchorder);
+// router.get('/order/:orderId', checkusersstatus, fetchparticularorder);
+// router.put('/cancelorder/:userId', checkusersstatus, deleteorder);
+// router.delete('/deleteitem/:userId', deleteitem);
+// router.put('/updatestatus/:id', updateStatus);
+// router.get('/fetchorderstatus/:userId', sortoptionorders);
+
+// // Wishlist Routes
+// router.post('/addwishlist', addwishlist);
+// router.get('/fetchwishlist/:userId', checkusersstatus, fetchwishlist);
+// router.delete('/removeproductfrowwishlist/:userId', removeproductfrowwishlist);
+
+// // Coupon Routes
+// router.get('/fetchcoupon/:userId', fetchcoupon);
+// router.get('/coupon/:couponId', couponamount);
+// router.post('/applycoupon/:userId', applycoupon);
+// router.get('/fetchcoupondetails/:couponId', checkusersstatus, coupondetails);
+
+// // Wallet Routes
+// router.get('/fetchwallet/:userId', checkusersstatus, fetchwallet);
+// router.post('/placeorderbywallet', placeorderbywallet);
+
+// // Payment Routes
+// router.post('/verifypayment', checkusersstatus, verifyPayment);
+// router.post('/verifyretrypayment', checkusersstatus, verifyretrypayment);
+// router.post('/preverifypayment', checkusersstatus, preverifypayment);
+// router.post('/updateProductQuantities', checkusersstatus, retryupdateproduct);
+// router.post('/createrazorpay', createrazorpay);
+// router.post('/verify', verifypaymentwallet);
+
+// // Return Order
+// router.put('/returnorder/:userId', returnorder);
+
+// // Search (Rate Limiter Applied)
+// router.get('/searchquery', searchLimiter, searchoption);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const express=require('express')
 const router=express.Router()
 const {verifypaymentwallet,createrazorpay,placeorderbywallet,couponamount,preverifypayment,products,retryupdateproduct,verifyretrypayment,fetchparticularorder,sortoptionorders,searchoption,fetchwallet,coupondetails,applycoupon,fetchcoupon,returnorder,removeproductfrowwishlist,fetchwishlist,verifyPayment,updatepasswordemail,forgetpasswordresendotp,forgetpasswordverifyotp,checkemail,updateuserdetail,deleteitem,deleteorder,checkout,placingorder,fetchdefaultaddress,changepassword,updateStatus,deleteaddress,updateaddress,fetechspecificaddress,fetchaddress,addaddress,updatecartminus,updatecartplus,fetchcart,addcart,refreshToken,categoryname,fetchrecom,getProducts,signup,verifyotp,resendotp,googleLogin, login, fetchproductdetails, fetchorder, addwishlist}=require('./Controller/userController')
